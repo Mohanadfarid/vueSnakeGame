c408bdb0579cb63603e068567072e13b371499e0
Mohanad Farid <106895779+Mohanadfarid@users.noreply.github.com>
2025-08-02 13:40:15 +0300

    fixed responsive

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts"></script>
 <template>
-  <button class="bg-blue-500 text-white rounded-4xl text-sm font-bold px-3 cursor-pointer active:scale-90">
+  <button class="bg-blue-500 text-white rounded-4xl font-bold py-1 px-3 cursor-pointer active:scale-90">
     <slot/>
   </button>
 </template>
--- src/components/SnakeGame.vue
+++ src/components/SnakeGame.vue
@@ -17,6 +17,6 @@ onMounted(() => {
     <p>snake length: {{ board.snakeTilesPosition.length  }} / {{ settings.boardSize * settings.boardSize  }}</p>
   </div>
 
-  <SnakeGameBoard class="max-w-[390px] pt-1 mx-auto shadow" />
+  <SnakeGameBoard class="max-w-[650px] pt-1 mx-auto shadow pb-2" />
 </template>
 <style></style>
